1fabb2668e456f2349714164775c4e992938e408
sdfksko <noua026@gmail.com>
2025-08-14 21:43:10 +0900

    spring_react_login 및 readme.md파일 수정

---
+++ spring_react_login/backend/src/main/java/com/example/backend/domain/user/dto/UserRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.backend.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+}
---
+++ spring_react_login/backend/src/main/java/com/example/backend/domain/user/service/UserService.java
@@ -0,0 +1,97 @@
+package com.example.backend.domain.user.service;
+
+import com.example.backend.domain.user.dto.UserRequestDTO;
+import com.example.backend.domain.user.entity.UserEntity;
+import com.example.backend.domain.user.entity.UserRoleType;
+import com.example.backend.domain.user.repository.UserRepository;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // 자체 로그인 회원 가입 (존재 여부)
+    @Transactional(readOnly = true)
+    public Boolean existUser(UserRequestDTO dto) {
+        return userRepository.existsByUsername(dto.getUsername());
+    }
+
+    // 자체 로그인 회원 가입
+    @Transactional
+    public Long addUser(UserRequestDTO dto) {
+
+        if(userRepository.existsByUsername(dto.getUsername())) {
+            throw new IllegalArgumentException("이미 유저가 존재합니다.");
+        }
+
+        UserEntity entity = UserEntity.builder()
+                .username(dto.getUsername())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .isLock(false)
+                .isSocial(false)
+                .roleType(UserRoleType.USER) // 우선 일반 유저로 가입
+                .nickname(dto.getNickname())
+                .email(dto.getEmail())
+                .build();
+
+        return userRepository.save(entity).getId();
+    }
+
+    // 자체 로그인
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity entity = userRepository.findByUsernameAndIsLockAndIsSocial(username, false, false)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return User.builder()
+                .username(entity.getUsername())
+                .password(entity.getPassword())
+                .roles(entity.getRoleType().name())
+                .accountLocked(entity.getIsLock())
+                .build();
+    }
+
+    // 자체 로그인 회원 정보 수정
+    @Transactional
+    public Long updateUser(UserRequestDTO dto) throws AccessDeniedException {
+
+        // 본인만 수정 기능 검증
+        String sessionUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(!sessionUsername.equals(dto.getUsername())) {
+            throw  new AccessDeniedException("본인 계정만 수정 가능");
+        }
+
+        // 조회
+        UserEntity entity = userRepository.findByUsernameAndIsLockAndIsSocial(dto.getUsername(), false, false)
+                .orElseThrow(() -> new UsernameNotFoundException(dto.getUsername()));
+
+        // 회원 정보 수정
+        entity.updateUser(dto);
+
+        return userRepository.save(entity).getId();
+    }
+
+    // 자체/소셜 로그인 회원 탈퇴
+
+    // 소셜 로그인 (매 로그인 시: 신규 = 가입, 기존 = 업데이트
+
+    // 자체/소셜 유저 정보 조회
+}
